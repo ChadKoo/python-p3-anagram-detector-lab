92995519e3427ac86368740dca1a120cb4580ad3
Chadwick Koo <chadwickkoo@outlook.com>
2025-05-13 06:17:42 +0300

    Modified files

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word=word
+   
+
+    def match (self, wordList):
+         
+        anagrams = [] 
+
+        for candidate in wordList:
+            if sorted(candidate) == sorted(self.word):
+                anagrams.append(candidate)
+
+        return anagrams
+
+       
+
